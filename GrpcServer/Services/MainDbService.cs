9537c80d1eeee4b9302bd26f2e6a67c52283275e
Каплюк Владислав Олегович <kuplykvlad@gmail.com>
2020-10-25 16:55:15 +0200

    Added first service

---
+++ GrpcServer/Services/MainDbService.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcServer
+{
+    public class MainDbService: CommonDbService.CommonDbServiceBase
+    {
+        private readonly ILogger<MainDbService> _logger;
+        public MainDbService(ILogger<MainDbService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
