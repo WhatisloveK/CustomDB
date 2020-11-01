55f1732ee3b894250e602af124e45c265bfba983
Каплюк Владислав Олегович <kuplykvlad@gmail.com>
2020-11-01 19:52:43 +0200

    mat-table modifications

--- grpc-dbclient/src/app/mat-table/mat-table.component.ts
+++ grpc-dbclient/src/app/mat-table/mat-table.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
+import {grpc} from '@improbable-eng/grpc-web';
+import {EntityService,DBService} from '../generated/dbEngine_pb_service';
+import {SelectRequest,SelectReply, GetEntityRequest, GetEntityReply} from "../generated/dbEngine_pb"
+import { environment } from 'src/environments/environment';
+import { HelperService } from '../services/helper.service';
 
 @Component({
   selector: 'app-mat-table',
@@ -12,16 +17,49 @@ export class MatTableComponent implements OnInit {
 
   tableDataSrc: any;
   // tslint:disable-next-line: no-input-rename
-  @Input('tableColumns') tableCols: string[];
-  @Input() tableData: {}[] = [];
+  @Input() tableName: string;
+  @Input() dbName: string;
+  tableCols: string[];
+  tableData: {}[] = [];
 
   @ViewChild(MatSort, { static: true }) sort: MatSort;
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
 
-  constructor() { }
+  constructor(private helperService: HelperService) { }
 
   ngOnInit() {
+    let entityRequest = new GetEntityRequest()
+    entityRequest.setDbname(this.dbName);
+    entityRequest.setTablename(this.tableName);
+    grpc.unary(EntityService.GetEntity, {
+      request: entityRequest,
+      host: environment.host, 
+      onEnd: res => {
+        const { status, message } = res;
+        if (status === grpc.Code.OK && message) {
+          var result = message.toObject() as GetEntityReply.AsObject;
+          this.tableCols = result.columnsList.map((item)=>item.name);
+        }
+      }
+    });
+
+    var selectRequest = new SelectRequest();
+    selectRequest.setTablename(this.tableName);
+    selectRequest.setDbname(this.dbName);
+    selectRequest.setShowsyscolumns(true);
+    grpc.unary(EntityService.Select, {
+      request: selectRequest,
+      host: environment.host, 
+      onEnd: res => {
+        const { status, message } = res;
+        if (status === grpc.Code.OK && message) {
+          var result = message.toObject() as SelectReply.AsObject;
+          this.tableData = result.rowsList.map((item)=> this.helperService.fromArrayToObject(item.itemsList, this.tableCols));
+        }
+      }
+    });
+    
     this.tableDataSrc = new MatTableDataSource(this.tableData);
     this.tableDataSrc.sort = this.sort;
     this.tableDataSrc.paginator = this.paginator;
