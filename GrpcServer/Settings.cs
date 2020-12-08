6986ca4fe40f874f04a4ca25cab339f50ffa7bd0
Каплюк Владислав Олегович <kuplykvlad@gmail.com>
2020-12-08 20:44:37 +0200

    configured startup

---
+++ GrpcServer/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer
+{
+    public class Settings
+    {
+        public string MongoDbConnectionString  { get; set; }
+        public string SqlServerConnectionString { get; set; }
+        public string JSONRootPath { get; set; }
+    }
+}
